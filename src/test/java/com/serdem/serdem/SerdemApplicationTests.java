3df53833b964d30186f8da4593a334bfdde8e9d0
M.Akram-c <M.Akram-c@LT-RUH-F26-0081.mshome.net>
2024-11-21 18:26:51 +0300

    Adding all files for service registry

---
+++ src/test/java/com/serdem/serdem/SerdemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serdem.serdem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerdemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
